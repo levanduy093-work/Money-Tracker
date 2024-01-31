06856f26563b5e3c14cc0d2c20a4a624a01987de
levanduy093-work <levanduy.work@gmail.com>
2024-01-31 10:33:56 +0700

    Initial Commit

---
+++ Money Tracker/Money_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Money_TrackerApp.swift
+//  Money Tracker
+//
+//  Created by Lê Văn Duy on 31/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Money_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
